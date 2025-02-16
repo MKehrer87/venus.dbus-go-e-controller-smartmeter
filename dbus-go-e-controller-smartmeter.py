6dcd0c24dca73ca76b3097986cb0f4766d2a20db
martinkehrer@t-online.de <martinkehrer@t-online.de>
2025-02-16 07:50:36 +0000

    Add Config and phase Sswitch

--- dbus-go-e-controller-smartmeter.py
+++ dbus-go-e-controller-smartmeter.py
@@ -9,14 +9,17 @@ This code and its documentation can be found on: https://github.com/RalfZim/venu
 Used https://github.com/victronenergy/velib_python/blob/master/dbusdummyservice.py as basis for this service.
 Reading information from the Fronius Smart Meter via http REST API and puts the info on dbus.
 """
-try:
-  import gobject  # Python 2.x
-except:
-  from gi.repository import GLib as gobject # Python 3.x
-import platform
+import platform 
 import logging
 import sys
 import os
+import sys
+if sys.version_info.major == 2:
+  import gobject  
+else:
+  from gi.repository import GLib as gobject
+
+import sys
 import requests # for http GET
 try:
   import thread   # for daemon = True  / Python 2.x
@@ -30,9 +33,20 @@ from vedbus import VeDbusService
 path_UpdateIndex = '/UpdateIndex'
 
 
-class DbusDummyService:
-  def __init__(self, servicename, deviceinstance, paths, productname='go-e controller Smart Meter', connection='go-e controller Smart Meter service'):
-    self._dbusservice = VeDbusService(servicename)
+class DbusGoeControllerService:
+  def __init__(self, servicename, deviceinstance, paths, productname='Go-E Smart Meter', connection='go-e controller Smart Meter service'):
+    config = self._getConfig()
+    self._accessType = config['DEFAULT']['AccessType']
+    self._host = config['DEFAULT']['Host'];
+    if accessType == 'ModBus':
+       logging.info("Access via Modbus")
+    else:
+       logging.info("Access via Http")
+       self._url = "http://%s/api/status?filter=isv,ccp,usv,cec,fwv" % (self._host)
+    self._SetL1 = config['DEFAULT'].getint('PhaseL1',1);
+    self._SwithcL2L3 = config['DEFAULT'].getboolean('SwitchL1L2',False);
+    
+    self._dbusservice = VeDbusService(servicename,register=False)
     self._paths = paths
 
     logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
@@ -49,19 +63,39 @@ class DbusDummyService:
     self._dbusservice.add_path('/FirmwareVersion', 0.1)
     self._dbusservice.add_path('/HardwareVersion', 0)
     self._dbusservice.add_path('/Connected', 1)
+    self._dbusservice.add_path('/Latency', None)
 
     for path, settings in self._paths.items():
       self._dbusservice.add_path(
-        path, settings['initial'], writeable=True, onchangecallback=self._handlechangedvalue)
+        path, settings['initial'], gettextcallback=settings['textformat'], writeable=True, onchangecallback=self._handlechangedvalue)
+
+    self._dbusservice.register()
 
     gobject.timeout_add(1000, self._update) # pause 1000ms before the next request
+    
+    # add _signOfLife 'timer' to get feedback in log every 5minutes
+    # 
+    # if config['DEFAULT'].getint('SignOfLifeLog',0)>0:
+    # gobject.timeout_add(config['DEFAULT'].getint('SignOfLifeLog')*60*1000, self._signOfLife)
+
+  def _getConfig(self):
+    config = configparser.ConfigParser()
+    config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
+    return config
+
+  def _signOfLife(self):
+    logging.info("--- Start: sign of life ---")
+    logging.info("Last _update() call: %s" % (self._lastUpdate))
+    logging.info("Last '/Ac/Power': %s" % (self._dbusservice['/Ac/Power']))
+    logging.info("--- End: sign of life ---")
+    return True
 
   def _update(self):
     try:
-      meter_url = "http://192.168.178.47/api/status?filter=isv,ccp,usv,cec,fwv"
+      #meter_url = "http://192.168.8.181/api/status?filter=isv,ccp,usv,cec,fwv"
 
       
-      meter_r = requests.get(url=meter_url) # request data from the Fronius PV inverter
+      meter_r = requests.get(url=self._url) # request data from the Fronius PV inverter
       meter_data = meter_r.json() # convert JSON data
 #        fwv = (meter_data['fwv'])
 #      self._dbusservice.add_path('/FirmwareVersion', fwv )
@@ -73,16 +107,67 @@ class DbusDummyService:
 #        meter_data['Body']['Data']['Current_AC_Phase_3'] = 0
 #        meter_data['Body']['Data']['PowerReal_P_Phase_2'] = 0
 #        meter_data['Body']['Data']['PowerReal_P_Phase_3'] = 0
+#      print("Power=",round((meter_data['ccp'][0]),2))
+
       self._dbusservice['/Ac/Power'] = round((meter_data['ccp'][0]),2) # positive: consumption, negative: feed into grid
-      self._dbusservice["/Ac/L1/Voltage"] = round((meter_data['usv'][0]['u1']),2)
-      self._dbusservice['/Ac/L2/Voltage'] = round((meter_data['usv'][0]['u2']),2)
-      self._dbusservice['/Ac/L3/Voltage'] = round((meter_data['usv'][0]['u3']),2)
-      self._dbusservice['/Ac/L1/Current'] = round((meter_data['isv'][0]['i']),2)
-      self._dbusservice['/Ac/L2/Current'] = round((meter_data['isv'][1]['i']),2)
-      self._dbusservice['/Ac/L3/Current'] = round((meter_data['isv'][2]['i']),2)
-      self._dbusservice['/Ac/L1/Power'] = round((meter_data['isv'][0]['p']),2)
-      self._dbusservice['/Ac/L2/Power'] = round((meter_data['isv'][1]['p']),2)
-      self._dbusservice['/Ac/L3/Power'] = round((meter_data['isv'][2]['p']),2)
+      if self._SetL1==2:      
+        self._dbusservice["/Ac/L1/Voltage"] = round((meter_data['usv'][0]['u2']),2)
+        self._dbusservice['/Ac/L1/Current'] = round((meter_data['isv'][1]['i']),2)
+        self._dbusservice['/Ac/L1/Power'] = round((meter_data['isv'][1]['p']),2)
+        if self._SwitchL2L3:
+          self._dbusservice['/Ac/L2/Voltage'] = round((meter_data['usv'][0]['u3']),2)
+          self._dbusservice['/Ac/L2/Current'] = round((meter_data['isv'][2]['i']),2)
+          self._dbusservice['/Ac/L2/Power'] = round((meter_data['isv'][2]['p']),2)
+          self._dbusservice['/Ac/L3/Voltage'] = round((meter_data['usv'][0]['u1']),2)
+          self._dbusservice['/Ac/L3/Current'] = round((meter_data['isv'][0]['i']),2)
+          self._dbusservice['/Ac/L3/Power'] = round((meter_data['isv'][0]['p']),2)
+        else:
+          self._dbusservice['/Ac/L2/Voltage'] = round((meter_data['usv'][0]['u1']),2)
+          self._dbusservice['/Ac/L2/Current'] = round((meter_data['isv'][0]['i']),2)
+          self._dbusservice['/Ac/L2/Power'] = round((meter_data['isv'][0]['p']),2)
+          self._dbusservice['/Ac/L3/Voltage'] = round((meter_data['usv'][0]['u3']),2)
+          self._dbusservice['/Ac/L3/Current'] = round((meter_data['isv'][2]['i']),2)
+          self._dbusservice['/Ac/L3/Power'] = round((meter_data['isv'][2]['p']),2)
+      elif self._SetL1==3:     
+        self._dbusservice["/Ac/L1/Voltage"] = round((meter_data['usv'][0]['u3']),2)
+        self._dbusservice['/Ac/L1/Current'] = round((meter_data['isv'][2]['i']),2)
+        self._dbusservice['/Ac/L1/Power'] = round((meter_data['isv'][2]['p']),2)        
+        if self._SwitchL2L3:
+          self._dbusservice['/Ac/L2/Voltage'] = round((meter_data['usv'][0]['u2']),2)
+          self._dbusservice['/Ac/L2/Current'] = round((meter_data['isv'][1]['i']),2)
+          self._dbusservice['/Ac/L2/Power'] = round((meter_data['isv'][1]['p']),2)
+          self._dbusservice['/Ac/L3/Voltage'] = round((meter_data['usv'][0]['u1']),2)
+          self._dbusservice['/Ac/L3/Current'] = round((meter_data['isv'][0]['i']),2)
+          self._dbusservice['/Ac/L3/Power'] = round((meter_data['isv'][0]['p']),2)
+        else:
+          self._dbusservice['/Ac/L2/Voltage'] = round((meter_data['usv'][0]['u1']),2)
+          self._dbusservice['/Ac/L2/Current'] = round((meter_data['isv'][0]['i']),2)
+          self._dbusservice['/Ac/L2/Power'] = round((meter_data['isv'][0]['p']),2)
+          self._dbusservice['/Ac/L3/Voltage'] = round((meter_data['usv'][0]['u2']),2)
+          self._dbusservice['/Ac/L3/Current'] = round((meter_data['isv'][1]['i']),2)
+          self._dbusservice['/Ac/L3/Power'] = round((meter_data['isv'][1]['p']),2)
+      else:
+        self._dbusservice["/Ac/L1/Voltage"] = round((meter_data['usv'][0]['u1']),2)
+        self._dbusservice['/Ac/L1/Current'] = round((meter_data['isv'][0]['i']),2)
+        self._dbusservice['/Ac/L1/Power'] = round((meter_data['isv'][0]['p']),2)        
+        if self._SwitchL2L3:
+          self._dbusservice['/Ac/L2/Voltage'] = round((meter_data['usv'][0]['u3']),2)
+          self._dbusservice['/Ac/L2/Current'] = round((meter_data['isv'][2]['i']),2)
+          self._dbusservice['/Ac/L2/Power'] = round((meter_data['isv'][2]['p']),2)
+          self._dbusservice['/Ac/L3/Voltage'] = round((meter_data['usv'][0]['u2']),2)
+          self._dbusservice['/Ac/L3/Current'] = round((meter_data['isv'][1]['i']),2)
+          self._dbusservice['/Ac/L3/Power'] = round((meter_data['isv'][1]['p']),2)
+        else:
+          self._dbusservice['/Ac/L2/Voltage'] = round((meter_data['usv'][0]['u2']),2)
+          self._dbusservice['/Ac/L2/Current'] = round((meter_data['isv'][1]['i']),2)
+          self._dbusservice['/Ac/L2/Power'] = round((meter_data['isv'][1]['p']),2)
+          self._dbusservice['/Ac/L3/Voltage'] = round((meter_data['usv'][0]['u3']),2)
+          self._dbusservice['/Ac/L3/Current'] = round((meter_data['isv'][2]['i']),2)
+          self._dbusservice['/Ac/L3/Power'] = round((meter_data['isv'][2]['p']),2)
+        
+#      self._dbusservuce['/ac/L1/PowerFactor'] = round((meter_data['isv'][0]['f']),3)
+#      self._dbusservice['/ac/L2/PowerVactor'] = round((meter_data['isv'][1]['f']),3)
+#      self._dbusservice['/ac/L3/PowerFactor'] = round((meter_data['isv'][2]['f']),3)
       self._dbusservice['/Ac/Energy/Forward'] = round(((meter_data['cec'][0][0])/1000),4)
       self._dbusservice['/Ac/Energy/Reverse'] = round(((meter_data['cec'][0][1])/1000),4)
 #      logging.info("House Consumption: {:.0f}".format(meter_consumption))
@@ -108,23 +193,48 @@ def main():
   # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
   DBusGMainLoop(set_as_default=True)
 
-  pvac_output = DbusDummyService(
+  #formatting
+  def _kwh(p, v):
+   return str("%.2f" % v) + "kWh"
+
+  def _a(p, v):
+   return str("%.1f" % v) + "A"
+
+  def _w(p, v):
+   return str("%i" % v) + "W"
+
+  def _v(p, v):
+   return str("%.2f" % v) + "V"
+
+  def _hz(p, v):
+   return str("%.4f" % v) + "Hz"
+
+  def _n(p, v):
+   return str("%i" % v)
+
+  pvac_output = DbusGoeControllerService(
     servicename='com.victronenergy.grid.mymeter',
     deviceinstance=0,
     paths={
-      '/Ac/Power': {'initial': None},
-      '/Ac/L1/Voltage': {'initial': None},
-      '/Ac/L2/Voltage': {'initial': None},
-      '/Ac/L3/Voltage': {'initial': None},
-      '/Ac/L1/Current': {'initial': None},
-      '/Ac/L2/Current': {'initial': None},
-      '/Ac/L3/Current': {'initial': None},
-      '/Ac/L1/Power': {'initial': None},
-      '/Ac/L2/Power': {'initial': None},
-      '/Ac/L3/Power': {'initial': None},
-      '/Ac/Energy/Forward': {'initial': None}, # energy bought from the grid
-      '/Ac/Energy/Reverse': {'initial': None}, # energy sold to the grid
-      path_UpdateIndex: {'initial': 0},
+      '/Ac/Power': {'initial': 0, 'textformat': _w},
+      '/Ac/L1/Voltage': {'initial': 0, 'textformat': _v},
+      '/Ac/L2/Voltage': {'initial': 0, 'textformat': _v},
+      '/Ac/L3/Voltage': {'initial': 0, 'textformat': _v},
+      '/Ac/L1/Current': {'initial': 0, 'textformat': _a},
+      '/Ac/L2/Current': {'initial': 0, 'textformat': _a},
+      '/Ac/L3/Current': {'initial': 0, 'textformat': _a},
+      '/Ac/L1/Power': {'initial': 0, 'textformat': _w},
+      '/Ac/L2/Power': {'initial': 0, 'textformat': _w},
+      '/Ac/L3/Power': {'initial': 0, 'textformat': _w},
+#      '/Ac/L1/Frequency' : {"initial": None, "textformat": _hz},
+#      '/Ac/L2/Frequency' : {"initial": None, "textformat": _hz},
+#      '/Ac/L3/Frequency' : {"initial": None, "textformat": _hz},
+#      '/ac/L1/PowerFactor': {'initial': None, 'textformat': _n},
+#      '/ac/L2/PowerFactor': {'initial': None, 'textformat': _n},
+#      '/ac/L3/PowerFactor': {'initial': None, 'textformat': _n}, 
+      '/Ac/Energy/Forward': {'initial': 0, 'textformat': _kwh}, # energy bought from the grid
+      '/Ac/Energy/Reverse': {'initial': 0, 'textformat': _kwh}, # energy sold to the grid
+      path_UpdateIndex: {'initial': 0, 'textformat': _n},
     })
 
   logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
